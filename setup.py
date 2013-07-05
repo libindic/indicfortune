c33d78e6e6ee57d0d12d3f07653927ee5870a4be
Diadara <nithin111@gmail.com>
2013-07-05 19:09:51 +0530

    initial commit fortune module ported

---
+++ setup.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+name = "fortune"
+
+setup(
+    name=name,
+    version="0.2.1",
+    url="http://silpa.org.in/sort",
+    license="LGPL-3.0",
+    description="Returns random quotes.",
+    author="Santhosh Thottingal",
+    author_email="santhosh.thottingal@gmail.com",
+    long_description="""Returns random proverbs and quotes from
+    chanakya, Malayalam proverbs and thrirukkural """,
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=['setuptools-git'],
+    install_requires=['setuptools', 'silpa_common'],
+    zip_safe=False,
+)
