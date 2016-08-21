b12be22a6ebcfeb0b3c83c15556d526ced4c470c
Jerin Philip <monu1618@gmail.com>
2016-08-22 00:29:47 +0530

    WSME REST Capability
    setup.py, setup.cfg : pbr, entry_points
    indicfortune/expose.py : REST functions
    indicfortune/__init__.py : Relative imports

---
+++ indicfortune/expose.py
@@ -0,0 +1,7 @@
+from .core import Fortune
+
+def indicfortune_fortune(database, pattern=None):
+    return Fortune().fortune(database, pattern)
+
+def fortune():
+    return [indicfortune_fortune, str, str, str]
--- setup.py
+++ setup.py
@@ -1,23 +1,8 @@
-# coding: utf-8
 #!/usr/bin/env python
-
-from setuptools import setup, find_packages
-
-name = "indicfortune"
+from setuptools import setup
 
 setup(
-    name=name,
-    version="0.2",
-    license="LGPL-3.0",
-    description="Returns random quotes.",
-    author="Santhosh Thottingal",
-    author_email="santhosh.thottingal@gmail.com",
-    long_description="""Returns random proverbs and quotes from
-    chanakya, Malayalam proverbs and thrirukkural """,
-    packages=find_packages(),
-    include_package_data=True,
-    setup_requires=['setuptools-git'],
-    install_requires=['setuptools' ],
-    test_suite='tests',
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True
 )
+
